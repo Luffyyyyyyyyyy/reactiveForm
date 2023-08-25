9c387a27199544918657f108a9659c33701307fb
officialpraveen2004 <officialpraveen2004p@gmail.com>
2023-08-25 17:47:31 +0530

    form initilizetion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,6 +24,7 @@ export class AppComponent {
       email: ['', [Validators.required]],
       password: ['', [Validators.required]],
       phoneNumber: ['', [Validators.required]],
+      moblieNumber: ['', [Validators.required]],
     })
   }
   public get name(): any {
